a42cb84a54f37b86749927398fff543817a5c1ee
George Francis <george.francis@gamer-network.net>
2018-12-18 15:48:20 +0000

    added hmr support

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { hot } from 'react-hot-loader/root'
 
 class App extends Component {
   render() {
@@ -9,4 +10,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default hot(App)
