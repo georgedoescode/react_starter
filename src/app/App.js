d93a51d2020320cff41bf214374d38abbd26831c
George Francis <george.francis@gamer-network.net>
2018-12-18 16:19:22 +0000

    remove test assets

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react';
 import { hot } from 'react-hot-loader/root'
 
-import webpackLogo from '../assets/images/webpack-logo.jpg';
-
 class App extends Component {
   render() {
     return (
       <div>
-        <img src={webpackLogo}></img>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 
 import './css/reset.css';
-import './css/typography.css';
 
 ReactDOM.render(
   <App></App>,
