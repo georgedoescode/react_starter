0d7a80a9776fdba4a78abc03f9375c413e316033
George Francis <george.francis@gamer-network.net>
2018-12-18 15:23:07 +0000

    added css support

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = env => {
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: '[name].bundle.js'
-    },
-    devServer: {
-      contentBase: 'dist',
-      overlay: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin('dist'),
-      new HtmlWebpackPlugin({
-        template: './static/index.html'
-      }),
-      new CopyWebpackPlugin([
-        { from: './static/favicon-32x32.png', to: './'}
-      ])
+const development = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  devServer: {
+    contentBase: 'dist',
+    overlay: true
+  },
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          development ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist'),
+    new HtmlWebpackPlugin({
+      template: './static/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+    new CopyWebpackPlugin([{
+      from: './static/favicon-32x32.png',
+      to: './'
+    }])
+  ]
 }
