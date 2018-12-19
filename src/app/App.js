eb467928e6d26535ebed7f298652f49736e60ec8
George Francis <george.francis@gamer-network.net>
2018-12-19 09:34:55 +0000

    fixed some more eslint errors

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
 const App = () => (
-  <div />
+  <div>
+    <p>React Starter</p>
+  </div>
 );
 
 export default hot(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const development = process.env.NODE_ENV === 'development';
@@ -12,12 +12,12 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
   },
   devtool: sourceMap,
   devServer: {
     contentBase: 'dist',
-    overlay: true
+    overlay: true,
   },
   module: {
     rules: [
@@ -25,46 +25,46 @@ module.exports = {
         enforce: 'pre',
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader'
+        loader: 'eslint-loader',
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
         use: [
           development ? 'style-loader' : MiniCssExtractPlugin.loader,
           'css-loader',
-          'postcss-loader'
-        ]
+          'postcss-loader',
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader'
-        ]
+          'file-loader',
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader'
-        ]
-      }
-    ]
+          'file-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin('dist'),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "css/[name].css",
+      filename: 'css/[name].css',
     }),
     new CopyWebpackPlugin([{
       from: './public/favicon-32x32.png',
-      to: './'
-    }])
-  ]
-}
+      to: './',
+    }]),
+  ],
+};
