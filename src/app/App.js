9dbeed04aeaadff99f6bb94cd16f94c10a4889d0
George Francis <george.francis@gamer-network.net>
2018-12-19 09:40:45 +0000

    added font smoothing to reset and test title

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-family: Helvetica, sans-serif;
+  color: rgba(0,0,0,0.85);
+  font-size: 48px;
+`;
 
 const App = () => (
   <div>
-    <p>React Starter</p>
+    <Title>React Starter</Title>
   </div>
 );
 
